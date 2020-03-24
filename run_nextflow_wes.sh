4f48adadea6027e4bda28e2acde5af2671c07808
rernst <r.f.ernst-3@umcutrecht.nl>
2020-03-24 17:11:37 +0100

    Add (s)bash run script

---
+++ run_nextflow_wes.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Set input and output dirs
+input=`realpath $1`
+output=`realpath $2`
+email=$3
+mkdir -p $output && cd $output
+
+sbatch <<EOT
+#!/bin/bash
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --mem 5G
+#SBATCH --gres=tmpspace:10G
+#SBATCH -o slurm_nextflow_wes.out
+#SBATCH -e slurm_nextflow_wes.err
+#SBATCH --mail-user $email
+#SBATCH --mail-type FAIL
+
+module load Java/1.8.0_60
+
+/hpc/diaggen/software/tools/nextflow run /hpc/diaggen/projects/workflow/DxNextflowWES/WES.nf \
+-c /hpc/diaggen/projects/workflow/DxNextflowWES/WES.config \
+--fastq_path $input \
+--outdir $output \
+-profile slurm \
+-with-notification $email \
+-resume -ansi-log false
+EOT
