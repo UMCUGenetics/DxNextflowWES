d370d18fc5a64565ba3dcb62cbf0a21fb265ca44
rernst <r.f.ernst-3@umcutrecht.nl>
2020-05-06 16:05:44 +0200

    Update path to production

--- run_nextflow_wes.sh
+++ run_nextflow_wes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-workflow_path='/hpc/diaggen/software/development/DxNextflowWES'
+workflow_path='/hpc/diaggen/software/production/DxNextflowWES'
 
 # Set input and output dirs
 input=`realpath -e $1`
