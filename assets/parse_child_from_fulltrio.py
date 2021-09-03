6bb7e6bde813d520c6403d37ad41b01546b9ca99
melferink <m.elferink@umcutrecht.nl>
2021-09-03 15:28:13 +0200

    first commit

---
+++ assets/parse_child_from_fulltrio.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+import argparse
+from check_kinship import parse_ped
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Check kinship output based on ped file.')
+    parser.add_argument('ped_file', type=argparse.FileType('r'), help='PED file')
+    parser.add_argument('vcf_files', nargs='+', help='VCF files (space seperated)')
+    arguments = parser.parse_args()
+
+    samples = parse_ped(arguments.ped_file)
+    trio_sample = []
+    for sample in samples:
+        if len(samples[sample]['parents']) == 2:
+            for vcf in arguments.vcf_files:
+                if sample in vcf:
+                    trio_sample.append(sample)
+
+    print(",".join(trio_sample))
