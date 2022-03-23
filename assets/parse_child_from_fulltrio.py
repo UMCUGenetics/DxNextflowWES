6a45aaccd2c6a7c0f334b9c6b843b45c54fdaab9
melferink <m.elferink@umcutrecht.nl>
2022-03-23 10:29:13 +0100

    update/refactor

--- assets/parse_child_from_fulltrio.py
+++ assets/parse_child_from_fulltrio.py
@@ -5,15 +5,15 @@ from check_kinship import parse_ped
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Check kinship output based on ped file.')
     parser.add_argument('ped_file', type=argparse.FileType('r'), help='PED file')
-    parser.add_argument('vcf_files', nargs='+', help='VCF files (space seperated)')
+    parser.add_argument('samples_analysis', nargs='+', help='samples within the anlysis (space seperated)')
     arguments = parser.parse_args()
 
     samples = parse_ped(arguments.ped_file)
     trio_sample = []
     for sample in samples:
-        if len(samples[sample]['parents']) == 2:
-            for vcf in list(set(arguments.vcf_files)):
-                if sample in vcf:
+        if len(samples[sample]['parents']) == 2:  # Sample = child with parents
+            for sample_analysis in list(set(arguments.samples_analysis)):
+                if sample in sample_analysis:
                     trio_sample.append(sample)
 
     print(",".join(trio_sample))
